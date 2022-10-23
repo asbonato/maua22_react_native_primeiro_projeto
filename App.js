d24f2df26ff71527fe1cfd0ac023ad985233f105
asbonato <asbonato@gmail.com>
2022-10-23 00:48:32 -0300

    Apostila 4 completa

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{contador}</Text>
+      <Button title="Alterar Texto" 
+              onPress={() => {setContador(contador + 1)}}/>
     </View>
   );
-}
+} 
+
+/* export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      contador: 0
+    }
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+      <Text>{this.state.contador}</Text>
+      <Button 
+        title="Alterar Texto" 
+        onPress={() => {this.setState({contador: this.state.contador + 1})}}
+      />
+    </View>
+    );
+  }
+} */
 
 const styles = StyleSheet.create({
   container: {
